1b6c0e9a93b48ee5788616b17bf0d6859ace8e3e
Alicia Yu <alicia.yu.16@gmail.com>
2017-09-27 06:34:55 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,12 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-class Parrot(Squawk)
-  def parrot=(phrase)
-    @phrase=phrase
-  end
-
-
-  def parrot(phrase = Squawk)
-    puts "#{phrase}!"
-  end
+def parrot(phrase = "Squawk")
+  puts "#{phrase}!"
 end
+
