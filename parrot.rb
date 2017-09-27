5a1b8ad611e915c8640c57715580615902174ee0
Alicia Yu <alicia.yu.16@gmail.com>
2017-09-27 06:46:16 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -4,7 +4,7 @@
 #The #parrot method should accept an argument of a string and both #puts out that string and return the string at the end of the method.
 #The #parrot method should have a default argument of "Squawk!".
 
-def parrot(phrase = "Squawk!")
+def parrot(phrase="Squawk!")
   puts phrase
   return phrase
 end
